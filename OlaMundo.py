ad902d23d7959cf7220a7573a1a5f5a9a0c8fe8b
Alexandre Junior <souzajr.alexandre@gmail.com>
2026-05-05 21:05:52 -0300

    Exemplo de condicional

--- OlaMundo.py
+++ OlaMundo.py
@@ -1,3 +1,11 @@
 nome = input('Digite seu nome:')
 
+idade = int(input("Digite sua idade: "))
+
 print(f'Ola, {nome}!')
+
+if idade >= 18:
+    print("Você é maior de idade!")
+else:
+    print("Você é menor de idade!")
+    
